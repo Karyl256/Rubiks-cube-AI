55858b39389443105c91865d8f31cddd484f622b
Karyl <tymczasowymailkarolatymczasowy@gmail.com>
2025-05-05 17:29:27 +0200

    WIP: Slight cleanup, added scramble, solve check

--- src/cube.rs
+++ src/cube.rs
@@ -1,4 +1,5 @@
 use colored::*;
+use rand::Rng;
 use std::fmt;
 
 pub struct Cube {
@@ -18,6 +19,61 @@ impl Cube {
     }
 }
 
+impl Cube {
+    pub fn scramble(&mut self, moves_count: i32) {
+        let mut rng = rand::rng();
+        let mut last_move: i32 = rng.random_range(0..=11);
+
+        //Avoiding the opposite variant of move, XOR swaps parity of last_move
+        let mut avoided_move: i32 = last_move ^ 1;
+
+        for _ in 0..moves_count {
+            let mut new_move = rng.random_range(0..11);
+            if new_move >= avoided_move { new_move += 1; }
+            last_move = new_move;
+            avoided_move = last_move ^ 1;
+
+            match new_move {
+                0 => self.u(),
+                1 => self.u_reverse(),
+                2 => self.l(),
+                3 => self.l_reverse(),
+                4 => self.f(),
+                5 => self.f_reverse(),
+                6 => self.r(),
+                7 => self.r_reverse(),
+                8 => self.b(),
+                9 => self.b_reverse(),
+                10 => self.d(),
+                11 => self.d_reverse(),
+                _ => print!("Scrambling Went wrong")
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn check_if_solved(&self) -> bool {
+        self.data.iter().take(6).all(|face| {
+            let face_color = face[4];
+            face.iter().all(|&color| color == face_color)
+        })
+    }
+}
+
+impl Default for Cube {
+    fn default() -> Self {
+        Self {
+            data: [
+                [Color::Yellow; 9],
+                [Color::Blue; 9], 
+                [Color::Red; 9], 
+                [Color::Green; 9], 
+                [Color::Orange; 9], 
+                [Color::White; 9]
+            ]
+        }
+    }
+}
 
 #[derive(PartialEq, Eq, Default, Clone, Copy, Debug)]
 pub enum Color {
@@ -43,21 +99,6 @@ impl fmt::Display for Color {
             Color::Blue => write!(f, "{}", "  ".on_blue()),
             #[cfg(test)]
             Color::Debug(i) => write!(f, "{:2}", i),
-        }
-    }
-}
-
-impl Default for Cube {
-    fn default() -> Self {
-        Self {
-            data: [
-                [Color::Yellow; 9],
-                [Color::Blue; 9], 
-                [Color::Red; 9], 
-                [Color::Green; 9], 
-                [Color::Orange; 9], 
-                [Color::White; 9]
-            ]
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,42 +5,7 @@ use cube::Cube;
 
 fn main() {
     let mut cube = Cube::default();
-    cube.print(); println!();
-    
-    cube.f();
-    cube.print(); println!();
-    cube.l();
-    cube.print(); println!();
-    cube.f();
-    cube.print(); println!();
-    cube.u_reverse();
-    cube.print(); println!();
-    cube.r();
-    cube.print(); println!();
-    cube.u();
-    cube.print(); println!();
-    cube.f();
-    cube.print(); println!();
-    cube.f();
-    cube.print(); println!();
-    cube.l();
-    cube.print(); println!();
-    cube.l();
-    cube.print(); println!();
-    cube.u_reverse();
-    cube.print(); println!();
-    cube.l_reverse();
-    cube.print(); println!();
-    cube.b();
-    cube.print(); println!();
-    cube.d_reverse();
-    cube.print(); println!();
-    cube.b_reverse();
-    cube.print(); println!();
-    cube.l();
-    cube.print(); println!();
-    cube.l();
-    cube.print(); println!();
-    cube.u();
-    cube.print(); println!();
+    cube.print();
+    cube.scramble(3);
+    cube.print();
 }
